d192dbe0a74cb49216418a06b6c73fed3255a482
barrettpoth <barrett.a.poth@gmail.com>
2017-03-22 18:02:37 -0500

    Added models

---
+++ bevo/bevo/Models/Dispute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace team1_longhornBank.Models
+{
+    public enum DisputeStatus
+    {
+        Submitted,
+        Accepted,
+        Rejected,
+        Adjusted
+    }
+    public class Dispute
+    {
+        public Int32 DisputeID { get; set; }
+
+        public DisputeStatus DisputeStatus { get; set; }
+
+        public String Message { get; set; }
+
+        public Decimal DipustedAmount { get; set; }
+    }
+}
