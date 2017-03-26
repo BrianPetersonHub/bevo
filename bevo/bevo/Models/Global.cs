26d8f9fc2c046709693ec937a4fbf7e4ea26b0cf
annaawang <anna.a.wang@gmail.com>
2017-03-26 10:58:11 -0700

    All int32 capitalized

--- bevo/bevo/Models/Global.cs
+++ bevo/bevo/Models/Global.cs
@@ -6,7 +6,8 @@ using System.Web;
 namespace bevo.Models
 {
     //global Account ID variable, unique amongst all Account types
-    public int32 AccountID;
+    public Int32 AccountID;
+
     public class Global
     {
     }
