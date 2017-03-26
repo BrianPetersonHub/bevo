b96e8e7c7ff73289c4b3a59b74fdc60989afbfed
annaawang <anna.a.wang@gmail.com>
2017-03-26 10:48:38 -0700

    Fixed int32

--- bevo/bevo/Models/CheckingAccount.cs
+++ bevo/bevo/Models/CheckingAccount.cs
@@ -7,7 +7,7 @@ namespace bevo.Models
 {
     public class CheckingAccount
     {
-        public int32 AccountID { get; set; }
+        public Int32 AccountID { get; set; }
         public String AccountName { get; set; }
         public Decimal Balance { get; set; }
     }
