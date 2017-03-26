5e3606eda60132336e15fa03e37cee4233cc534c
barrettpoth <barrett.a.poth@gmail.com>
2017-03-26 13:18:03 -0500

    Added DataAnnotation to account classes

--- bevo/bevo/Models/SavingAccount.cs
+++ bevo/bevo/Models/SavingAccount.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace bevo.Models
 {
     public class SavingAccount
     {
         public Int32 AccountID { get; set; }
+
+        [DisplayName(Name = "Account Name")]
         public String AccountName { get; set; }
         public Decimal Balance { get; set; }
     }
