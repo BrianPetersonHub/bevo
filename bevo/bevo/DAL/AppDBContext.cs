be6fb26d1ee628766c6d4c06a37e42ac0e692c5c
barrettpoth <barrett.a.poth@gmail.com>
2017-03-29 17:24:53 -0500

    Added DAL folder and DBContext class

---
+++ bevo/bevo/DAL/AppDBContext.cs
@@ -0,0 +1,23 @@
+﻿using bevo.Models;
+using System.Data.Entity;
+
+namespace bevo.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        //Constructor that invokes the base constructor
+        public AppDbContext() : base("DBConnection") { }
+
+        //Create the db set
+        public DbSet<CheckingAccount> CheckingAccounts { get; set; }
+        public DbSet<Dispute> Disputes { get; set; }
+        public DbSet<IRAccount> IRAccounts { get; set; }
+        public DbSet<Payee> Payees { get; set; }
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<SavingAccount> SavingAccounts { get; set; }
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<StockDetail> StockDetail { get; set; }
+        public DbSet<StockPortfolio> StockPortfolio { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
