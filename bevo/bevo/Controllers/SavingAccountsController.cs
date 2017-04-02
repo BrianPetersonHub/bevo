360f9214547ad3aec726c79ea51997459d56d8c9
annaawang <anna.a.wang@gmail.com>
2017-04-02 11:06:25 -0700

    Saving Accounts Controller

---
+++ bevo/bevo/Controllers/SavingAccountsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using bevo.Models;
+
+namespace bevo.Controllers
+{
+    public class SavingAccountsController : Controller
+    {
+        public SavingAccountsController()
+        {
+            SavingAccount SavingAccount = new SavingAccount();
+            SavingAccount.AccountNum = 123;
+            SavingAccount.AccountName = "Longhorn Savings";
+            SavingAccount.Balance = 0;
+        }
+
+        public SavingAccountsController(Decimal balance)
+        {
+
+        }
+
+        public SavingAccountsController(String accountName)
+        {
+
+        }
+
+        // GET: SavingAccounts
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
