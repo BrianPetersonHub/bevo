ca3ca2c457256db6f1b20101b3378bd30cec91c0
barrettpoth <barrett.a.poth@gmail.com>
2017-04-02 12:55:16 -0500

    Started working on PersonController

---
+++ bevo/bevo/Controllers/CheckingAccountsController.cs
@@ -0,0 +1,31 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+//using System.Web.Mvc;
+//using bevo.Models;
+
+//namespace bevo.Controllers
+//{
+//    public class CheckingAccountsController : Controller
+//    {
+//        public CheckingAccountsController()
+//        {
+//            CheckingAccount CheckingAccount = new CheckingAccount();
+//            CheckingAccount.AccountNum = 0;
+//            CheckingAccount.AccountName = "LonghornBank";
+//        }
+
+//        // GET: CheckingAccounts
+//        public ActionResult Index()
+//            {
+//                return View();
+//            }
+        
+//            public Int32 CountAccounts()
+//            {
+//                Int32 intNumAccounts;
+//                intNumAccounts += CheckingAccount.
+//            }
+//    }
+//}
---
+++ bevo/bevo/Controllers/PersonsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using bevo.DAL;
+using bevo.Models;
+namespace bevo.Controllers
+{
+    public class PersonsController : Controller
+    {
+        private AppDbContext db = new AppDbContext();
+
+        // GET: Persons
+        public ActionResult Index()
+        {
+            return View(); 
+        }
+
+        //GET: Persons/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //POST: Persons/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PersonID,FirstName,MiddleInitial,LastName,Street,City,State,ZipCode,Email,PhoneNumber,BirthDay,Password")] Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Persons.Add(person);
+                db.SaveChanges();
+                return RedirectToAction("ChooseAccount");
+            }
+            return View(person);
+        }
+
+        public ActionResult ChooseAccount()
+        {
+            return View();
+        }
+    }
+}
