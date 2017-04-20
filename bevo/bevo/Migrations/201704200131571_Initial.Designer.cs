cbb850ca333cad247513d825ee572f6a188b20b8
annaawang <anna.a.wang@gmail.com>
2017-04-19 18:42:27 -0700

    new migration

--- bevo/bevo/Migrations/201704200131571_Initial.Designer.cs
+++ bevo/bevo/Migrations/201704200131571_Initial.Designer.cs
@@ -7,13 +7,13 @@ namespace bevo.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class UpdatedSeedMethod : IMigrationMetadata
+    public sealed partial class Initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(UpdatedSeedMethod));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201704182006409_Updated Seed Method"; }
+            get { return "201704200131571_Initial"; }
         }
         
         string IMigrationMetadata.Source
